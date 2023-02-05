e8032afbb3ca65c3b9b7419d5b9b14928245be23
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-05 12:36:41 +0200

    Create a Navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center p-5">
+      Navbar
+    </nav>
+  );
+};
+
+export default Navbar;
