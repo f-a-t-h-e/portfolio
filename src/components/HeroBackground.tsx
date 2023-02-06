eeb8c3edeca5460f200bad5ac8e5f74a6bdfdde4
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-06 04:12:18 +0200

    Center the background.

--- src/components/HeroBackground.tsx
+++ src/components/HeroBackground.tsx
@@ -18,11 +18,11 @@ const HeroBackground = (props: Props) => {
       }}
       className="relative flex justify-center items-center"
     >
-      <div className="absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
-      <div className="absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52 " />
-      <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52 " />
-      <div className="absolute border border-[#FAABAA] rounded-full opacity-20 h-[650px] w-[650px] mt-52 animate-pulse" />
-      <div className="absolute border border-[#333333] rounded-full h-[800px] w-[800px] mt-52 " />
+      <div className="bag absolute border border-yellow-500 rounded-full h-[200px] w-[200px]  animate-ping" />
+      <div className="absolute border border-yellow-800 rounded-full h-[300px] w-[300px] " />
+      <div className="absolute border border-yellow-900 rounded-full h-[500px] w-[500px]  " />
+      <div className="absolute border border-yellow-800 rounded-full opacity-20 h-[650px] w-[650px]  animate-pulse" />
+      <div className="absolute border border-yellow-900 rounded-full h-[800px] w-[800px]  " />
       <div className="" />
     </motion.div>
   );
