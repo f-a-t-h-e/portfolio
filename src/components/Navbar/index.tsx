894339ccdf5312d98eff3255531530580b07544a
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-06 03:34:02 +0200

    Update navbar.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <nav className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center p-5">
-      Navbar
+    <nav className="hidden md:flex justify-center pt-5 sticky top-0 z-[21] mb-[40vh] h-0">
+      <Link href="#about">
+        <button className="heroButton">About</button>
+      </Link>
+      <Link href="#experience">
+        <button className="heroButton">Experience</button>
+      </Link>
+      <Link href="#skills">
+        <button className="heroButton">Skills</button>
+      </Link>
+      <Link href="#projects">
+        <button className="heroButton">Projects</button>
+      </Link>
     </nav>
   );
 };
