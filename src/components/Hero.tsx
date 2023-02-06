8556cd30fbcc350a642e4a059f392da8f80cced1
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-06 03:34:23 +0200

    Add Navbar.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,8 +18,10 @@ const Hero = (props: Props) => {
     delaySpeed: 2000,
   });
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+    <div className="translate-y-[-50%] flex flex-col space-y-8 items-center justify-center text-center ">
+      {/* <div className="absolute top-[50] right-[50] translate-y-[-50%] translate-x-[-50%]"> */}
       <HeroBackground />
+      {/* </div> */}
       <div className="w-32 h-32 mx-auto relative">
         <Image
           className="rounded-full object-cover"
@@ -35,25 +37,10 @@ const Hero = (props: Props) => {
         <h2 className="text-sm uppercase text-gray-500 pd-2 tracking-[1rem]">
           Fullstack developer
         </h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10 mb-6">
           <span className="-mr-3">{text}</span>
           <Cursor cursorColor="#7ABBA" />
         </h1>
-
-        <div className="pt-5">
-          <Link href="#about">
-            <button className="heroButton">About</button>
-          </Link>
-          <Link href="#experience">
-            <button className="heroButton">Experience</button>
-          </Link>
-          <Link href="#skills">
-            <button className="heroButton">Skills</button>
-          </Link>
-          <Link href="#projects">
-            <button className="heroButton">Projects</button>
-          </Link>
-        </div>
       </div>
     </div>
   );
